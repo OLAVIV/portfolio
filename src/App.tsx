c0bf0655f0d31e29a20e57b448b6841445dee147
Olavi Kurki <olavikurki@Ohjelmointi-16-MacBook-Pro.local>
2021-10-11 14:30:54 +0300

    koti

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import React, { useState } from 'react'
 import ClickCount from './components/clickCount';
-import SomeExampleComponent from './components/example/someExampleComponent';
-import MyFirstComponent from './components/myFirstComponent';
+import styles from "./App.module.scss"
 
 const App: React.FC = () => {
+  const [toggle, setToggle] = useState<boolean>(false)
   return (
-    <div>
-      <SomeExampleComponent />
-      <ClickCount/>
-    </div>
+    // Important! add your repositoryname to basename
+    <Router basename='/portfolio'>
+      <div>
+      <p className= {styles.otsikko}> My Portfolio</p>
+      <button className= {styles.btncolor} onClick= {() => setToggle(!toggle)}>
+      MY Achievements
+      </button>
+
+      {
+        toggle
+          ?(
+            <div>
+              <h1><a href="https://github.com/OLAVIV" target="_blank"><button className= {styles.achi}>My github profile</button></a></h1>
+              <h2><a href="https://github.com/alextoro306/SnakeFighters" target="_blank"><button className= {styles.achi}>Snake Fighters</button></a></h2>
+            </div>
+          )
+          :null
+          
+      }
+
+
+      </div>
+    </Router>
+
   )
 }
 
