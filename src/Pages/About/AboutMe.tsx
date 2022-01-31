61b1aa421906be16391134287a4a26b05ee60f2f
olaVI <olavi.kurki2004@gmail.com>
2022-01-31 16:18:53 +0200

    Portfolio1.9

--- src/Pages/About/AboutMe.tsx
+++ src/Pages/About/AboutMe.tsx
@@ -12,11 +12,15 @@ const AboutMe: React.FC<AboutMeProps> = () => {
   return (
     <div className={ useDarkmode ? styles.dark : styles.light}>
         <div>
-            {/* <p className={styles.otsikko}>My Portfolio</p> */}
             <p className={useDarkmode ? styles.aboutmedark : styles.aboutmelight}>Hi, my name is Olavi and I am an information and communications (ICT) student from Vantaa Finland.</p>
             <p className={useDarkmode ? styles.aboutmedark : styles.aboutmelight}>I am currently studying at Varia Myyrmäki, I've studied for about one and a half years by now.</p>
             <p className={useDarkmode ? styles.aboutmedark : styles.aboutmelight}>The things i've learnt are the following: Basics of Java, JavaScript, TypeScript, React, CSS and SCSS</p>
             <p className={useDarkmode ? styles.aboutmedark : styles.aboutmelight}>I've studied the basics of Unity, and C# scripting for Unity quite thoroughly, but there is always something new to learn.</p>
+            <div className="mapouter">
+              <div className={styles.maps}>
+                <iframe title="Google maps" width={411} height={400} id="gmap_canvas" src="https://maps.google.com/maps?q=Ojahaantie%209&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0}/>
+                </div>
+                </div>
         </div>
         <Link to='/' className= {useDarkmode ? styles.buttondark : styles.buttonlight} >Back</Link>
     </div>
--- src/Pages/mycv/MyCv.tsx
+++ src/Pages/mycv/MyCv.tsx
@@ -11,7 +11,6 @@ const MyCv: React.FC<MyCvProps> = () => {
   document.title="My CV"
   return (
     <div className={ useDarkmode ? styles.dark : styles.light}>
-        {/* <p className={styles.otsikko}>My Portfolio</p> */}
         <p className={useDarkmode ? styles.aboutmedark : styles.aboutmelight}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget scelerisque neque, quis scelerisque erat. Quisque venenatis molestie sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi. Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat ipsum vel vulputate ultricies.
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget scelerisque neque, quis scelerisque erat. Quisque venenatis molestie sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi. Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat ipsum vel vulputate ultricies.
